10944d38a619051cd911147aade1ec39bb21e37d
neeiva <104152120+neeiva@users.noreply.github.com>
2022-05-04 18:29:27 -0300

    Versão final do nosso projeto

---
+++ src/main/java/com/dio/santander/banklineapi/dto/NovoCorrentista.java
@@ -0,0 +1,19 @@
+package com.dio.santander.banklineapi.dto;
+
+public class NovoCorrentista {
+	private String nome;
+	private String cfp;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCfp() {
+		return cfp;
+	}
+	public void setCfp(String cfp) {
+		this.cfp = cfp;
+	}
+	
+}
--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -26,6 +26,10 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name ="id_conta")
+	private Integer IdConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -56,6 +60,11 @@ public class Movimentacao {
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
-	
+	public Integer getIdConta() {
+		return IdConta;
+	}
+	public void setIdConta(Integer idConta) {
+		IdConta = idConta;
+	}
 
 }
